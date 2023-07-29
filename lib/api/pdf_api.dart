3ff5d7d7e3b903c695a4c16ebecad3d889550da0
Boukandoura Merouane <m_boukandoura@estin.dz>
2023-07-29 20:28:25 +0100

    first commit

---
+++ lib/api/pdf_api.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+import 'package:flutter/services.dart';
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart';
+
+class PdfApi {
+  static Future<File> saveDocument({
+    required String name,
+    required Document pdf,
+  }) async {
+    print("saving document");
+    final bytes = await pdf.save();
+
+    final dir = await getApplicationDocumentsDirectory();
+    final file = File('${dir.path}/$name');
+    print(file);
+    print("file");
+
+    await file.writeAsBytes(bytes);
+
+    return file;
+  }
+
+  static Future openFile(File file) async {
+    final url = file.path;
+
+    await OpenFile.open(url);
+  }
+}
---
+++ lib/backupMain.dart
@@ -0,0 +1,28 @@
+import 'package:bts_technologie/page/pdf_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static final String title = 'Invoice';
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: title,
+      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      home: PdfPage(),
+    );
+  }
+}
+
